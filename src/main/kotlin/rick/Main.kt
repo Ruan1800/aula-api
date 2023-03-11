13d8527f189d65ba96d294b2ba9abf9d44d22f5f
Ruan Ribeiro <Rrjalfim@gmail.com>
2023-03-10 21:20:20 -0300

    concluido

---
+++ src/main/kotlin/rick/Main.kt
@@ -0,0 +1,5 @@
+package rick
+
+fun main (){
+
+}
---
+++ src/main/kotlin/rick/rick.kt
@@ -0,0 +1,16 @@
+package rick
+
+
+data class character(
+    val name: String,
+    val status: String,
+    val species: String,
+    val gender: String,
+    val origin: List<String>,
+    val location: List<String>,
+    val episode: List<String>,
+)
+
+
+
+
